42aa76edbb7e950ed4c679411570b065f9bc69f3
franko-fox <git@frankofox.dev>
2022-12-15 17:32:12 +0100

    beautified the code a little, now using proper logging

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,24 @@
-mod chars;
-mod utils;
-
-use aes::cipher::{BlockEncrypt, KeyInit};
-use aes::{Aes128, Block};
-use btleplug::api::{Central, Manager as _, Peripheral as _, ScanFilter, WriteType};
-use btleplug::api::{CharPropFlags, Characteristic};
-use btleplug::platform::{Manager, Peripheral};
-use futures::stream::StreamExt;
 use std::error::Error;
 use std::time::Duration;
+
+use aes::{Aes128, Block};
+use aes::cipher::{BlockEncrypt, KeyInit};
+use anyhow::bail;
+use btleplug::api::{Central, Manager as _, Peripheral as _, PeripheralProperties, ScanFilter, WriteType};
+use btleplug::api::{Characteristic, CharPropFlags};
+use btleplug::platform::{Manager, Peripheral};
+use futures::stream::StreamExt;
+use log::{debug, error, info, warn};
 use tokio::time;
 use uuid::Uuid;
 
+
+mod chars;
+mod utils;
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    pretty_env_logger::init();
     println!("LynxHR Version v0.0.1 ALPHA");
 
     loop {
@@ -22,24 +27,53 @@ async fn main() -> Result<(), Box<dyn Error>> {
         // Get the first bluetooth adapter available by the Host Device
         // !! : If no adapters is found the program will crash!
         let adapters = manager.adapters().await?;
-        let central = adapters.into_iter().nth(0).unwrap();
+        if adapters.len() == 0 {
+            error!("No Bluetooth Adapter Found!");
+            return Ok(());
+        }
+        let central = adapters[0].clone();
 
         // Start scanning for devices
-        println!("Scanning for known devices...");
-        let scanf = ScanFilter {
+        info!("Scanning for known devices...");
+        let scanfilter = ScanFilter {
             services: vec![create_uuid!("0000fee0-0000-1000-8000-00805f9b34fb")],
         };
-        central.start_scan(scanf).await?;
+        if let Err(_) = central.start_scan(scanfilter).await {
+            error!("Failed to start scanning!");
+            return Ok(());
+        }
 
         time::sleep(Duration::from_secs(10)).await;
+        let devices: Vec<Peripheral>;
+        match central.peripherals().await {
+            Ok(d) => devices = d,
+            Err(_) => {
+                error!("Failed to get peripherals!");
+                return Ok(());
+            }
+        }
 
-        let devices = central.peripherals().await?;
-
-        println!("Available Devices -> {}", devices.len());
+        info!("Available Devices -> {}", devices.len());
 
         for device in devices.iter() {
-            let properties = device.properties().await?;
-            let is_connected = device.is_connected().await?;
+            let properties: Option<PeripheralProperties>;
+            match device.properties().await {
+                Ok(p) => properties = p,
+                Err(_) => {
+                    error!("Failed to get properties!");
+                    error!("Skipping device...");
+                    continue;
+                }
+            }
+            let is_connected: bool;
+            match device.is_connected().await {
+                Ok(c) => is_connected = c,
+                Err(_) => {
+                    error!("Failed to get connection status!");
+                    error!("Skipping device...");
+                    continue;
+                }
+            }
             let local_name = properties
                 .unwrap()
                 .local_name
@@ -47,33 +81,38 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
             /* Connect To Device */
             if !is_connected {
-                println!("Connecting to peripheral {:?}...", &local_name);
+                debug!("Connecting to peripheral {:?}...", &local_name);
                 if let Err(err) = device.connect().await {
-                    eprintln!("Error connecting to peripheral, skipping: {}", err);
+                    warn!("Error connecting to peripheral, skipping: {}", err);
                     continue;
                 }
             }
+            info!("Connected!");
+            info!("Warning! Authentication Key is required to access your device!");
 
-            let is_connected = device.is_connected().await?;
+            // Subscribe to Auth Char
+            if let Err(_) = subscribe_to_characteristic(&device, &chars::CHARS_UUIDS[..3], false).await {
+                error!("Failed to subscribe to Characteristics!");
+                error!("Skipping device...");
+                continue;
+            }
 
-            if is_connected {
-                println!("Connected!");
-                println!("Warning! Authentication Key is required to access your device!");
-
-                // Subscribe to Auth Char
-                subscribe_to_characteristic(&device, &chars::CHARS_UUIDS[..3], false).await?;
-
-                // Authentication Challange
-                let auth: Characteristic = create_char!(
+            // Authentication Challange
+            let auth: Characteristic = create_char!(
                     "00000009-0000-3512-2118-0009af100700",
                     "0000fee0-0000-1000-8000-00805f9b34fb",
                     (NOTIFY, WRITE_WITHOUT_RESPONSE)
                 );
 
-                authenticate(&device, &auth, &chars::CHARS_UUIDS[3..]).await?;
-
-                // Authentification Success Testing
+            if let Err(_) = authenticate(&device, &auth, &chars::CHARS_UUIDS[3..]).await {
+                error!("Failed to Authenticate!");
+                error!("Skipping device...");
+                continue;
             }
+
+
+            // Authentification Success Testing
+            hr_control_test(&device, &chars::HR_CONTROL).await?;
         }
     }
 }
@@ -107,10 +146,10 @@ async fn authenticate(
     device: &Peripheral,
     auth_char: &Characteristic,
     chars: &[Uuid],
-) -> Result<(), Box<dyn Error>> {
+) -> anyhow::Result<()>  {
     let auth_key: &[u8] = &0xc9a57d375d8d96ffd3331b73b123d43bu128.to_be_bytes();
 
-    println!("Starting Authentication...");
+    info!("Starting Authentication...");
 
     // Ask for Authentication
     device
@@ -120,27 +159,27 @@ async fn authenticate(
     // Get data from authentication notification
     let mut notification_stream = device.notifications().await?;
     while let Some(data) = notification_stream.next().await {
-        println!("Awaiting for Data...");
+        debug!("Awaiting for Data...");
 
-        println!("Received data from [{:?}]: {:?}", data.uuid, data.value);
+        debug!("Received data from [{:?}]: {:?}", data.uuid, data.value);
 
         // This means we received authentication information
         if data.uuid == auth_char.uuid {
-            println!("Debug : {:?}", &data.value[..3]);
+            debug!("Debug : {:?}", &data.value[..3]);
 
             if &data.value[..3] == [0x10, 0x02, 0x01] {
                 let random_number = &data.value[3..];
 
-                println!("Number Received : {:?}", random_number);
+                debug!("Number Received : {:?}", random_number);
 
-                println!("Encrypting...");
+                debug!("Encrypting...");
                 let cipher = Aes128::new_from_slice(&auth_key).unwrap();
 
                 // Create a block
                 let mut blk = Block::clone_from_slice(&random_number);
                 cipher.encrypt_block(&mut blk);
 
-                println!("Number Encrypted : {:?}", blk);
+                debug!("Number Encrypted : {:?}", blk);
 
                 let encrypted_number: &[u8] = blk.as_slice();
 
@@ -153,20 +192,22 @@ async fn authenticate(
                     )
                     .await?;
 
-                println!("info: sent AUTHENTIFICATION_CHALLANGE")
+                debug!("info: sent AUTHENTIFICATION_CHALLANGE")
             }
 
             if &data.value[..3] == [0x10, 0x03, 0x08] {
-                println!("error: AUTHENTICATION_FAILED");
+                error!("AUTHENTICATION_FAILED");
+                bail!("Authentication Failed!");
             }
 
             if &data.value[..3] == [0x10, 0x03, 0x01] {
-                println!("info: AUTHENTICATION_SUCCESS");
+                info!("AUTHENTICATION_SUCCESS");
 
                 // Initialize
-                subscribe_to_characteristic(&device, chars, true).await?;
-
-                hr_control_test(&device, &chars::HR_CONTROL).await?;
+                if let Err(_) = subscribe_to_characteristic(&device, chars, true).await {
+                    error!("Failed to subscribe to Characteristics!");
+                    bail!("Failed to subscribe to Characteristics!");
+                }
             }
         }
     }
@@ -175,7 +216,7 @@ async fn authenticate(
 }
 
 // Get Heart Rate
-async fn hr_control_test(device: &Peripheral, char: &Characteristic) -> Result<(), Box<dyn Error>> {
+async fn hr_control_test(device: &Peripheral, char: &Characteristic) -> anyhow::Result<()> {
     loop {
         // Force Heartrate
         device
@@ -188,9 +229,9 @@ async fn hr_control_test(device: &Peripheral, char: &Characteristic) -> Result<(
         // Read Heartrate
         let mut notification_stream = device.notifications().await?;
         while let Some(data) = notification_stream.next().await {
-            println!("Awaiting for Data...");
+            debug!("Awaiting for Data...");
 
-            println!("Received data from [{:?}]: {:?}", data.uuid, data.value);
+            debug!("Received data from [{:?}]: {:?}", data.uuid, data.value);
 
             let hr_mesure_uuid = create_uuid!("00002a37-0000-1000-8000-00805f9b34fb");
 
@@ -201,12 +242,12 @@ async fn hr_control_test(device: &Peripheral, char: &Characteristic) -> Result<(
             let time_data = device.read(&chars::TIME).await?;
 
             if data.uuid == hr_mesure_uuid {
-                println!("info: Current HR -> {:?}bpm", data.value.to_vec());
-                println!(
+                info!("info: Current HR -> {:?}bpm", data.value.to_vec());
+                info!(
                     "info: Current Battery -> Percentage : {:?}% | Charging : {:?}",
                     bat_data, 0
                 );
-                println!("info: Current Time -> {:?}", time_data);
+                info!("info: Current Time -> {:?}", time_data);
             }
         }
         device
