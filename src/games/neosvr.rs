1910fd1dae051df546bd700f3f1615dc4538fa03
Lynix <81735347+lunarlynix@users.noreply.github.com>
2022-12-15 10:39:23 -0400

    Commit Game Files

---
+++ src/games/neosvr.rs
@@ -0,0 +1,3 @@
+// NeosVR Specific Data Parsing / Socket Here
+// Type : Websocket
+// Parsing Format : value1;value2;value3;value4
---
+++ src/games/vrchat.rs
@@ -0,0 +1,11 @@
+// VRChat Specific Data Parsing / Socket Here
+// Type : OSC
+// Requires port : 9000 on 127.0.0.1 / 127.1
+// Parsing Format : 
+    /*
+        Send to OSC Address (address)
+        /avatar/parameters/Heartrate = BPM Heartrate (float)
+        /avatar/parameters/TimeHR = Time HR (int)
+        /avatar/parameters/TimeMIN = Time MIN (int)
+        /avatar/parameters/WatchBattery = Watch Batt (int)
+    */
